68e5a396d2f4ae4bdf1a7cd4b5928f4814d58f6b
riderway <riderway@runbox.com>
2024-12-23 21:11:46 +0100

    Add grades + activity graph

--- web/components/ui/progress.tsx
+++ web/components/ui/progress.tsx
@@ -1,36 +1,36 @@
 "use client"
 
-import * as React from "react"
 import * as ProgressPrimitive from "@radix-ui/react-progress"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+function getDefaultIndicatorClassName(value: number) {
+  if (value === 100) return "bg-green-400"
+  if (value > 100) return "bg-red-400"
+  return "bg-primary"
+}
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
+    indicatorClassName?: string
+  }
+>(({ className, value, indicatorClassName, ...props }, ref) => {
   const percent = value || 0
 
   return (
     <ProgressPrimitive.Root
       ref={ref}
-      className={cn(
-        "relative h-1.5 w-full overflow-hidden rounded-full bg-primary/20",
-        className,
-      )}
+      className={cn("relative h-1.5 w-full overflow-hidden rounded-full bg-primary/20", className)}
       {...props}
     >
       <ProgressPrimitive.Indicator
-        className={cn("h-full w-full flex-1 rounded-full transition-all", {
-          "bg-red-400": percent > 100,
-          "bg-primary": percent < 100,
-          "bg-green-400": percent === 100,
-        })}
-        style={
-          percent < 100
-            ? { transform: `translateX(-${100 - percent}%)` }
-            : undefined
-        }
+        className={cn(
+          "h-full w-full flex-1 rounded-full transition-all",
+          indicatorClassName ?? getDefaultIndicatorClassName(percent),
+        )}
+        style={percent < 100 ? { transform: `translateX(-${100 - percent}%)` } : undefined}
       />
     </ProgressPrimitive.Root>
   )
