fea9146407f5a7f9e6d7f621a9256db457c49085
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-01-08 18:27:46 -0500

    cleanup + redeploy

---
+++ web/app/components/builder-section.tsx
@@ -0,0 +1,33 @@
+import "server-only"
+
+import { getUser, hasSession } from "@/lib/auth/user"
+import { ActionCard } from "./action-card/action-card"
+import { HomepageActivity } from "./homepage-activity"
+import { cn } from "@/lib/utils"
+import { Grant } from "@prisma/flows"
+
+export async function BuilderSection({ grants }: { grants: Grant[] }) {
+  const hasGrant = grants.length > 0
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-1 gap-2 rounded-2xl bg-gradient-to-b from-secondary to-secondary/80",
+        hasGrant ? "md:col-span-3 md:grid-cols-3" : "md:col-span-1 md:grid-cols-1",
+      )}
+    >
+      <div
+        className={cn(
+          "relative isolate overflow-hidden p-5 pb-6",
+          !hasGrant && "md:mx-auto md:w-full md:max-w-lg",
+        )}
+      >
+        <ActionCard user={await getUser()} hasSession={await hasSession()} />
+      </div>
+      {hasGrant && (
+        <div className="md:col-span-2">
+          <HomepageActivity grants={grants} />
+        </div>
+      )}
+    </div>
+  )
+}
--- web/app/components/flows-stories.tsx
+++ web/app/components/flows-stories.tsx
@@ -1,30 +1,17 @@
 import "server-only"
 
 import { StoryCard } from "@/app/components/story-card"
-import { getUser, hasSession } from "@/lib/auth/user"
-import database, { getCacheStrategy } from "@/lib/database/edge"
-import { ActionCard } from "./action-card/action-card"
+import database from "@/lib/database/edge"
 import { FeaturedStoryCard } from "./story-card-featured"
 import { ImpactDialog } from "@/app/item/[grantId]/components/impact-dialog"
-import { HomepageActivity } from "./homepage-activity"
-import { cn } from "@/lib/utils"
+import { BuilderSection } from "./builder-section"
 
-export async function FlowsStories({ recipient }: { recipient?: `0x${string}` }) {
-  const [stories, grants] = await Promise.all([
-    database.story.findMany({
-      where: { complete: true },
-      orderBy: { created_at: "desc" },
-      take: 7,
-      ...getCacheStrategy(600),
-    }),
-    recipient
-      ? database.grant.findMany({
-          where: { isActive: true, isTopLevel: false, recipient },
-          include: { derivedData: true },
-          ...getCacheStrategy(600),
-        })
-      : Promise.resolve([]),
-  ])
+interface Props {
+  recipient?: `0x${string}`
+}
+
+export async function FlowsStories({ recipient }: Props) {
+  const [stories, grants] = await getStoriesAndGrants(recipient)
 
   const hasGrant = grants.length > 0
 
@@ -36,26 +23,7 @@ export async function FlowsStories({ recipient }: { recipient?: `0x${string}` })
 
   return (
     <div className="mt-10 grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-      <div
-        className={cn(
-          "grid grid-cols-1 gap-2 rounded-2xl bg-gradient-to-b from-secondary to-secondary/80",
-          hasGrant ? "md:col-span-3 md:grid-cols-3" : "md:col-span-1 md:grid-cols-1",
-        )}
-      >
-        <div
-          className={cn(
-            "relative isolate overflow-hidden p-5 pb-6",
-            !hasGrant && "md:mx-auto md:w-full md:max-w-lg",
-          )}
-        >
-          <ActionCard user={await getUser()} hasSession={await hasSession()} />
-        </div>
-        {hasGrant && (
-          <div className="md:col-span-2">
-            <HomepageActivity grants={grants} />
-          </div>
-        )}
-      </div>
+      <BuilderSection grants={grants} />
 
       {hasGrant && <ImpactDialog grants={grants} dialogTitle="Your Impact" />}
 
@@ -66,4 +34,20 @@ export async function FlowsStories({ recipient }: { recipient?: `0x${string}` })
       ))}
     </div>
   )
+}
+
+function getStoriesAndGrants(recipient?: `0x${string}`) {
+  return Promise.all([
+    database.story.findMany({
+      where: { complete: true },
+      orderBy: { created_at: "desc" },
+      take: 7,
+    }),
+    recipient
+      ? database.grant.findMany({
+          where: { isActive: true, isTopLevel: false, recipient },
+          include: { derivedData: true },
+        })
+      : Promise.resolve([]),
+  ])
 }
