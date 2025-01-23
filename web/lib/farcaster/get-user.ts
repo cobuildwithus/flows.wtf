8096418f439334b507f3d179d35415541e18afb0
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-01-23 18:45:35 -0500

    gonzo is back!

--- web/lib/farcaster/get-user.ts
+++ web/lib/farcaster/get-user.ts
@@ -1,12 +1,13 @@
 import { farcasterDb } from "@/lib/database/farcaster-edge"
 import { getCacheStrategy } from "../database/edge"
+import { Profile as FarcasterProfile } from "@prisma/farcaster"
 
 export const getFarcasterUserByEthAddress = async (address: `0x${string}`) => {
   try {
     const users = await getFarcasterUsersByEthAddress(address)
     if (!users || users.length === 0) return null
 
-    return users[0]
+    return convertFarcasterUsers(users)[0]
   } catch (e: any) {
     console.error(e?.message)
     return null
@@ -26,7 +27,7 @@ export const getFarcasterUsersByEthAddress = async (rawAddress: `0x${string}`) =
       ...getCacheStrategy(86400),
     })
 
-    return users
+    return convertFarcasterUsers(users)
   } catch (e: any) {
     console.error(e?.message)
     return []
@@ -46,7 +47,7 @@ export const getFarcasterUsersByEthAddresses = async (addresses: `0x${string}`[]
       ...getCacheStrategy(3600),
     })
 
-    return users
+    return convertFarcasterUsers(users)
   } catch (e: any) {
     console.error(e?.message)
     return []
@@ -61,9 +62,25 @@ export const getFarcasterUsersByFids = async (fids: bigint[]) => {
       ...getCacheStrategy(3600),
     })
 
-    return users
+    return convertFarcasterUsers(users)
   } catch (e: any) {
     console.error(e?.message)
     return []
   }
+}
+
+// if this format
+//https://imagedelivery.net/BXluQx4ige9GuW0Ia56BHw/7e4c77e2-ceb6-4327-d083-cf0d5b624200/rectcrop3
+// then remove the rectcrop3 and replace with original
+// https://imagedelivery.net/BXluQx4ige9GuW0Ia56BHw/7e4c77e2-ceb6-4327-d083-cf0d5b624200/
+function getFarcasterPfpUrl(url: string | null) {
+  if (!url) return null
+  return url.replace("/rectcrop3", "/original")
+}
+
+function convertFarcasterUsers(users: FarcasterProfile[]): FarcasterProfile[] {
+  return users.map((user) => ({
+    ...user,
+    avatar_url: getFarcasterPfpUrl(user.avatar_url),
+  }))
 }
