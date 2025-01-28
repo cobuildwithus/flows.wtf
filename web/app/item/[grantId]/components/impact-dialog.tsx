c9b4090817fec43e6a183626fc8d6fd9cdb8d47b
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-01-28 01:01:13 -0500

    Update impact-dialog.tsx

--- web/app/item/[grantId]/components/impact-dialog.tsx
+++ web/app/item/[grantId]/components/impact-dialog.tsx
@@ -86,7 +86,7 @@ export function ImpactDialog(props: Props) {
             </div>
           ))}
         </div>
-        <div className="rounded-lg border border-muted/20 bg-muted/5 text-right text-xs text-muted-foreground">
+        <div className="text-right text-xs text-muted-foreground">
           {lowestScoringGrant.overallScore < 90
             ? "*Impact score updates every 2 days when updates are posted"
             : "*Impact score updates every 2 weeks"}
