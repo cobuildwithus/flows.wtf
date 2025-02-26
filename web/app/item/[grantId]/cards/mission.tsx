977441d7352ddae7f343e6da3cb50aca80f932d4
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-02-26 00:38:19 -0500

    simplify!

--- web/app/item/[grantId]/cards/mission.tsx
+++ web/app/item/[grantId]/cards/mission.tsx
@@ -14,7 +14,7 @@ export function MissionCard(props: Props) {
       <div className="flex flex-col items-start justify-between lg:h-full">
         <div className="items-top flex w-full justify-between gap-2">
           <Compass className="size-6" />
-          <div className="text-[11px] uppercase tracking-wider opacity-60">Core Mission</div>
+          <div className="text-[11px] uppercase tracking-wider opacity-60">Mission</div>
         </div>
         <ul className="mt-8 list-inside list-disc text-sm leading-normal">{text}</ul>
       </div>
