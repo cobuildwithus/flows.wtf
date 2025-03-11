500b1d571927c70650dcb0975fc03a3f7cae2a5e
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-03-10 21:27:08 -0400

    fix bug with counting grants

--- indexer/src/tcr/statuses.ts
+++ indexer/src/tcr/statuses.ts
@@ -45,11 +45,4 @@ async function handleItemStatusChange(params: {
     isResolved: _resolved,
     challengePeriodEndsAt,
   })
-
-  if (_itemStatus === Status.ClearingRequested) {
-    await context.db.update(grants, { id: _itemID }).set((row) => ({
-      challengedRecipientCount: row.challengedRecipientCount + 1,
-      awaitingRecipientCount: row.awaitingRecipientCount - 1,
-    }))
-  }
 }
