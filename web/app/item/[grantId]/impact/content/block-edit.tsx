be957b4d1c78a14b6cbce2a519c08f429ea17b78
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-03-22 21:16:52 -0600

    Update block-edit.tsx

--- web/app/item/[grantId]/impact/content/block-edit.tsx
+++ web/app/item/[grantId]/impact/content/block-edit.tsx
@@ -30,6 +30,7 @@ export function BlockEdit(props: Props) {
             if (confirmed) {
               setIsEditing(false)
             }
+            router.refresh()
           }}
         >
           Finish editing
