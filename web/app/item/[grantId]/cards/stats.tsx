e14a61be2de553eff7eb60bdf8f7409d27b8faae
riderway <riderway@runbox.com>
2025-04-02 18:17:58 +0200

    Display impact summary

--- web/app/item/[grantId]/cards/stats.tsx
+++ web/app/item/[grantId]/cards/stats.tsx
@@ -1,9 +1,10 @@
+import { cn } from "@/lib/utils"
 import { PropsWithChildren } from "react"
 
-export const Stat = (props: PropsWithChildren<{ label: string }>) => {
-  const { children, label } = props
+export const Stat = (props: PropsWithChildren<{ label: string; className?: string }>) => {
+  const { children, label, className } = props
   return (
-    <div className="col-span-6 rounded-xl border px-4 py-4 md:px-5 xl:col-span-3">
+    <div className={cn("col-span-6 rounded-xl border px-4 py-4 md:px-5 xl:col-span-3", className)}>
       <div className="flex h-[46px] items-center text-2xl font-bold lg:text-3xl">{children}</div>
       <div className="text-sm text-muted-foreground">{label}</div>
     </div>
