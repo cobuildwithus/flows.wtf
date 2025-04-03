e81c0ec828cab0c6e70ec9627c885739224ffb1f
riderway <riderway@runbox.com>
2025-04-03 20:25:54 +0200

    Minor prettier

--- web/components/ui/badge.tsx
+++ web/components/ui/badge.tsx
@@ -12,12 +12,12 @@ const badgeVariants = cva(
         secondary:
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         destructive:
-          "border-transparent bg-destructive text-destructive-foreground  hover:bg-destructive/80",
+          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
         success:
           "border-transparent bg-green-600 dark:bg-green-700 text-white hover:bg-green-600/80 dark:hover:bg-green-700/80",
         warning:
-          "border-transparent bg-yellow-400 dark:bg-yellow-500  text-yellow-950 hover:bg-yellow-400/80 dark:hover:bg-yellow-500/80",
+          "border-transparent bg-yellow-400 dark:bg-yellow-500 text-yellow-950 hover:bg-yellow-400/80 dark:hover:bg-yellow-500/80",
       },
     },
     defaultVariants: {
