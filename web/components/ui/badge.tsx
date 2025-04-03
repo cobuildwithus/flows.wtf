eacd89bf3d2bd738a83a8fcafa6349cd2479b667
riderway <riderway@runbox.com>
2025-04-03 20:25:54 +0200

    Minor prettier

--- web/components/ui/badge.tsx
+++ web/components/ui/badge.tsx
@@ -8,17 +8,16 @@ const badgeVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
+        default: "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
         secondary:
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         destructive:
-          "border-transparent bg-destructive text-destructive-foreground  hover:bg-destructive/80",
+          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
         success:
           "border-transparent bg-green-600 dark:bg-green-700 text-white hover:bg-green-600/80 dark:hover:bg-green-700/80",
         warning:
-          "border-transparent bg-yellow-400 dark:bg-yellow-500  text-yellow-950 hover:bg-yellow-400/80 dark:hover:bg-yellow-500/80",
+          "border-transparent bg-yellow-400 dark:bg-yellow-500 text-yellow-950 hover:bg-yellow-400/80 dark:hover:bg-yellow-500/80",
       },
     },
     defaultVariants: {
@@ -32,9 +31,7 @@ export interface BadgeProps
     VariantProps<typeof badgeVariants> {}
 
 function Badge({ className, variant, ...props }: BadgeProps) {
-  return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
-  )
+  return <div className={cn(badgeVariants({ variant }), className)} {...props} />
 }
 
 export { Badge, badgeVariants }
