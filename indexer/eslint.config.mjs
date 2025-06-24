ddbedb08f63d02375be863213ef417bc56ebc946
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-06-23 23:55:23 -0400

    cleanup eslint for indexer

--- indexer/eslint.config.mjs
+++ indexer/eslint.config.mjs
@@ -1,19 +1,51 @@
 import { defineConfig } from "eslint/config"
-import path from "node:path"
+import { dirname } from "node:path"
 import { fileURLToPath } from "node:url"
 import js from "@eslint/js"
 import { FlatCompat } from "@eslint/eslintrc"
+import globals from "globals"
 
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-})
+const __dirname = dirname(__filename)
+const compat = new FlatCompat({ baseDirectory: __dirname })
 
 export default defineConfig([
+  js.configs.recommended,
+
+  ...compat.extends("ponder"),
+
+  { ignores: ["eslint.config.*"] },
+
   {
-    extends: compat.extends("ponder"),
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+  },
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "no-undef": "off",
+
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+      "no-useless-escape": "off",
+    },
   },
 ])
