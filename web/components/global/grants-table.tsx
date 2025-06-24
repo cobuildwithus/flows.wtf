f963201eba089c36f7a2a99698fa90d92fd0613d
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-06-24 15:26:38 -0400

    minor cleanup

--- web/components/global/grants-table.tsx
+++ web/components/global/grants-table.tsx
@@ -42,7 +42,7 @@ export function GrantsTable(props: Props) {
           <TableHead className="text-center">Total earned</TableHead>
           <TableHead className="text-center">Monthly support</TableHead>
           <TableHead className="text-center">Votes</TableHead>
-          <TableHead className="text-right">{canManage ? "Manage payout" : "Your Vote"}</TableHead>
+          <TableHead className="text-right">{canManage ? "Manage" : "Your Vote"}</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
