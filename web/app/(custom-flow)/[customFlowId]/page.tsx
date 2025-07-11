16067dd2a2fa505584c32e52737db1b7e0986aff
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-07-11 19:39:55 -0400

    feat(web): dynamic startup redirects (#77)
    * feat(web): dynamic startup redirects
    
    * privacy and legal!
    
    * privacy/toc
    
    * fix

--- web/app/(custom-flow)/[customFlowId]/page.tsx
+++ web/app/(custom-flow)/[customFlowId]/page.tsx
@@ -2,9 +2,11 @@ import "server-only"
 
 import { getFlow } from "@/lib/database/queries/flow"
 import { Metadata } from "next"
-import { notFound } from "next/navigation"
+import { notFound, redirect } from "next/navigation"
 import { CustomFlowPage as CustomFlowPageComponent } from "../custom-flow-page"
 import { CustomFlowId, getCustomFlow } from "../custom-flows"
+import { getStartup, getStartupIdFromSlug } from "@/lib/onchain-startup/startup"
+import { getIpfsUrl } from "@/lib/utils"
 
 interface Props {
   params: Promise<{ customFlowId: string }>
@@ -12,6 +14,18 @@ interface Props {
 
 export async function generateMetadata(props: Props): Promise<Metadata> {
   const { customFlowId } = await props.params
+
+  const startupId = getStartupIdFromSlug(customFlowId)
+  if (startupId) {
+    const startup = await getStartup(startupId)
+
+    return {
+      title: startup.title,
+      description: startup.tagline,
+      openGraph: { images: [getIpfsUrl(startup.image, "pinata")] },
+    }
+  }
+
   const customFlow = getCustomFlow(customFlowId as CustomFlowId)
 
   if (!customFlow) notFound()
@@ -21,6 +35,10 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
 
 export default async function CustomFlowPage(props: Props) {
   const { customFlowId } = await props.params
+
+  const startupId = getStartupIdFromSlug(customFlowId)
+  if (startupId) return redirect(`/startup/${startupId}`)
+
   const customFlow = getCustomFlow(customFlowId as CustomFlowId)
 
   if (!customFlow) notFound()
