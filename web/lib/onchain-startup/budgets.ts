bb5b0e44563dd30a8aa40942662027caf0fce09c
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-07-16 11:12:20 -0400

    add hiring events to timeline

--- web/lib/onchain-startup/budgets.ts
+++ web/lib/onchain-startup/budgets.ts
@@ -23,6 +23,7 @@ async function _getStartupBudgets(id: string) {
     include: {
       subgrants: {
         where: { isActive: true },
+        omit: { description: true },
         include: { derivedData: true },
       },
     },
@@ -33,6 +34,8 @@ async function _getStartupBudgets(id: string) {
     title: budget.id === id ? "Main Budget" : budget.title,
   }))
 }
+
+export type StartupBudget = Awaited<ReturnType<typeof _getStartupBudgets>>[0]
 
 export const getStartupBudgets = unstable_cache(_getStartupBudgets, ["startup-budgets"], {
   tags: ["startup-budgets"],
